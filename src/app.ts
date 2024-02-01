33bf76746d7b56254379be177f20162024b2dfc3
YongLee <nogy21@gmail.com>
2024-02-01 19:23:05 +0900

    chore: lint, prettier 추가

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application } from 'express';
+import 'express-async-errors';
+import router from './route';
+
+const app: Application = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(router);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
