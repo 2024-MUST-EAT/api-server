88982dcc24d092b290bdf733e6ec09aa63bab656
Sir-Rain <liciaan@kakao.com>
2024-02-23 20:51:16 +0900

    add: signup code 인메모리 가입기능 테스트

--- src/domains/auth/routes.ts
+++ src/domains/auth/routes.ts
@@ -1,8 +1,24 @@
 import { Router } from 'express';
+import bcrypt from 'bcrypt';
 
 const router = Router();
 
+
+let userId = 1;
+const userTable: { [userId: number] : { email: string, password:string}} = {};
+
 router.post('/login', (req, res) => {});
-router.post('/signup', (req, res) => {});
+router.post('/signup', async (req, res) => {
+
+    // type safe를 위해 추후 validation 추가 예정
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+
+    const hashPassword = await bcrypt.hash(password, 12);
+
+    userTable[userId] = { email, password: hashPassword };
+    userId++;
+
+});
 
 export default router;
