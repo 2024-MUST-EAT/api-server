b615de228278bc37c6c51b9dbd25ec3503252e74
Sir-Rain <liciaan@kakao.com>
2024-02-24 02:00:24 +0900

    add: 간단 로그인 추가

--- src/domains/auth/routes.ts
+++ src/domains/auth/routes.ts
@@ -7,7 +7,33 @@ const router = Router();
 let userId = 1;
 const userRepository: { id: number, email: string, password: string}[] = [];
 
-router.post('/login', (req, res) => {});
+router.post('/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = userRepository.find(user => user.email = email);
+
+    if(!user) {
+        return new Error('Not Found User');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if( !isMatch ) {
+        return new Error('Invalid User Information') // 유저 비밀번호 오류 처리 필요
+    }
+
+    // Type Error 수정 필요
+    req.session.isLoggedIn = true;
+    req.session.user = user;
+
+    req.session.save((err) => {
+        // 세션 저장 오류 처리 필요
+        console.log(err);
+    })
+
+
+});
 router.post('/signup', async (req, res) => {
 
     // type safe를 위해 추후 validation 추가 예정
