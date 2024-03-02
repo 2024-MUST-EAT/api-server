241a070e7ac749571774651975e0d93900e9d521
Sir-Rain <liciaan@kakao.com>
2024-03-02 19:58:34 +0900

    refactor: auth controller 클래스화

--- src/domains/auth/auth.controller.ts
+++ src/domains/auth/auth.controller.ts
@@ -1,41 +1,42 @@
 import { Request, Response } from 'express';
 
 import { UserData } from './auth';
-import * as authService from './auth.service';
+import { AuthService } from './auth.service';
 
+export class AuthController {
+    constructor(private readonly authService: AuthService){}
 
-export const doLogin = async (req: Request, res: Response) => {
-    const userData: UserData = req.body;
+    doLogin = async (req: Request, res: Response) => {
+        const userData: UserData = req.body;
+    
+        try {
+            const user = await this.authService.doLogin(userData);
+    
+            // Type Error 수정 필요
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+    
+            req.session.save();
+    
+            res.status(200).json({message: 'Login Success', id: user.id});
+            return;
+        } catch( error ) {
+            res.status(500).json({message: 'Login Error'});
+        }
+    };
 
-    try {
-        const user = await authService.doLogin(userData);
-
-        // Type Error 수정 필요
-        req.session.isLoggedIn = true;
-        req.session.user = user;
-
-        req.session.save();
-
-        res.status(200).json({message: 'Login Success', id: user.id});
+    doSignup = async (req: Request, res: Response) => {
+        // type safe를 위해 추후 validation 추가 예정
+        const userData: UserData = req.body;
+        try {
+        const user = await this.authService.doSignup(userData);
+    
+        res.status(201).json({id: user.id, email: user.email});
         return;
-    } catch( error ) {
-        res.status(500).json({message: 'Login Error'});
-    }
+        } catch( error ) {
+            res.status(500).json({message: 'Signup Error'});
+        }
+        
     
-
-}
-
-export const doSignup = async (req: Request, res: Response) => {
-    // type safe를 위해 추후 validation 추가 예정
-    const userData: UserData = req.body;
-    try {
-    const user = await authService.doSignup(userData);
-
-    res.status(201).json({id: user.id, email: user.email});
-    return;
-    } catch( error ) {
-        res.status(500).json({message: 'Signup Error'});
     }
-    
-
 }
