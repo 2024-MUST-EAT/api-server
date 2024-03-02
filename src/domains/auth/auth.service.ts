2422cbb997035e6a7ec45bb0ac57972b9f62c971
Sir-Rain <liciaan@kakao.com>
2024-03-02 20:01:09 +0900

    refactor: auth service 클래스화

--- src/domains/auth/auth.service.ts
+++ src/domains/auth/auth.service.ts
@@ -1,46 +1,51 @@
 import bcrypt from 'bcrypt';
 
 import { UserData, User } from './auth';
-import * as authRepository from './auth.repository';
+import { AuthRepository } from './auth.repository';
 
-export const doLogin = async (userData: UserData): Promise<User> => {
-    const { email, password } = userData;
+export class AuthService{
+    constructor(private readonly authRepository: AuthRepository){}
 
-
-    try{
-        const user: User = await authRepository.findByEmail(email);
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if( !isMatch ) {
-            throw new Error('Invalid User Information') // 유저 비밀번호 오류 처리 필요
-        }
-
-        return user;
-    } catch( error ) {
-
-        throw error;
-    }
+    doLogin = async (userData: UserData): Promise<User> => {
+        const { email, password } = userData;
     
-}
-
-export const doSignup = async (userData: UserData): Promise<User> => {
-    const { email, password } = userData;
-
-
-    try {
-        const existUser = await authRepository.findByEmail(email);
-
-        if(existUser) {
-            throw new Error('Already Exist User');
-        }
-
-        const hashPassword = await bcrypt.hash(password, 12);
-        const user = await authRepository.registNewUser({email, password: hashPassword});
-
-        return user
-    } catch( error ) {
-        throw error
-    }
     
+        try{
+            const user: User = await this.authRepository.findByEmail(email);
+    
+            const isMatch = await bcrypt.compare(password, user.password);
+    
+            if( !isMatch ) {
+                throw new Error('Invalid User Information') // 유저 비밀번호 오류 처리 필요
+            }
+    
+            return user;
+        } catch( error ) {
+    
+            throw error;
+        }
+        
+    }
+
+    doSignup = async (userData: UserData): Promise<User> => {
+        const { email, password } = userData;
+    
+    
+        try {
+            const existUser = await this.authRepository.findByEmail(email);
+    
+            if(existUser) {
+                throw new Error('Already Exist User');
+            }
+    
+            const hashPassword = await bcrypt.hash(password, 12);
+            const user = await this.authRepository.registNewUser({email, password: hashPassword});
+    
+            return user
+        } catch( error ) {
+            throw error
+        }
+        
+    }
+
 }
