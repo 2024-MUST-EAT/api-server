6631c05d7e7d5d005ab4c1e4a3fb511f957e6bb7
YongLee <nogy21@gmail.com>
2024-03-12 20:43:29 +0900

    fix: 예상치 못 한 오류로 인해 checkExist 메서드 추가

--- src/domains/auth/auth.repository.ts
+++ src/domains/auth/auth.repository.ts
@@ -3,36 +3,37 @@ import { User, UserData } from './auth';
 let userId = 1;
 const userRepository: User[] = [];
 
-
 export class AuthRepository {
-    findByEmail = async(email: string): Promise<User> => {
+  checkExist = async (email: string): Promise<boolean> => {
+    return !!userRepository.find((user) => (user.email = email));
+  };
 
+  findByEmail = async (email: string): Promise<User> => {
+    try {
+      const user: User | undefined = userRepository.find(
+        (user) => (user.email = email),
+      );
 
-        try {
-            const user: User | undefined = userRepository.find(user => user.email = email);
-    
-            if(!user) {
-                throw new Error('Not found User');
-            }
-        return user;
-        } catch ( error ) {
-            throw error;
-        }
+      if (!user) {
+        throw new Error('Not found User');
+      }
+      return user;
+    } catch (error) {
+      throw error;
     }
+  };
 
-    registNewUser = async (userData: UserData): Promise<User> => {
-        const { email, password} = userData;
-    
-    
-        try {
-            const user: User = {id: userId, email: email, password: password };
-            userRepository.push(user);
-            userId++;
-    
-            return user;
-        } catch( error ) {
-            throw error
-        }
-        
+  registNewUser = async (userData: UserData): Promise<User> => {
+    const { email, password } = userData;
+
+    try {
+      const user: User = { id: userId, email: email, password: password };
+      userRepository.push(user);
+      userId++;
+
+      return user;
+    } catch (error) {
+      throw error;
     }
+  };
 }
